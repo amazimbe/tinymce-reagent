5ef98e442b9a41d324a106a9f9bf8c6870ae9db2
Alfred Mazimbe <alfred.mazimbe@gmail.com>
2017-09-21 11:41:18 +0100

    HIV-607 reagent plugin (#1)
    * added reagent plugin
    
    * completed reagent plugin
    
    * updated readme
    
    * include inventory in a reagent
    
    * applied code review feedback

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Welcome stranger! This is a repo containing the reagent TinyMCE plugin.
 
 ## The development server
 
-By running the `npm start` command you start the development server and open a browser window with an instance of TinyMCE with your plugin added to it. This window will reload automatically whenever a change is detected in the `index.html` file in the `static` folder or in one of the JavaScript files in the `src` directory.
+Checkout the code from Github and change into the plugin's directory. Install all npm dependencies by running `npm install`.
+Install all bower dependencies by running `bower install`. Run `npm start` to start the development server and open a browser window with an instance of TinyMCE with your plugin added to it. This window will reload automatically whenever a change is detected in the `index.html` file in the `static` folder or in one of the JavaScript files in the `src` directory.
 
 ## The production build
 
@@ -12,4 +13,4 @@ By running the `npm run build` command Webpack will create a `dist` directory wi
 
 * `plugin.js` - the bundled plugin
 * `plugin.min.js` - the bundles, uglified and minified plugin
-* `LICENSE` - a file explaining the license of your plugin (copied over from `src/LICENSE`) 
+* `LICENSE` - a file explaining the license of your plugin (copied over from `src/LICENSE`)
--- src/plugin.js
+++ src/plugin.js
@@ -1,20 +1,68 @@
-import _ from 'lodash';
+import Handlebars from 'handlebars';
 
 const plugin = (editor) => {
   editor.addButton('reagent', {
-    text: 'reagent',
-    icon: false,
-    onclick: () => {
-      // Open window
-      editor.windowManager.open({
-        title: 'reagent plugin',
-        body: [
-          { type: 'textbox', name: 'title' }
-        ],
+    tooltip: 'Reagent',
+    icon: 'reagent',
+
+    onclick() {
+      let window = editor.windowManager.open({
+        height: 112,
+        width: 460,
+        title: 'Insert reagent',
+
+        buttons: [{
+          text: 'Ok',
+          classes: 'widget btn primary',
+          onclick() {
+            window.submit();
+          }
+        }, {
+          text: 'Cancel',
+          onclick() {
+            window.close();
+          }
+        }],
+
+        body: [{
+          type: 'textbox',
+          name: 'reagent',
+          label: 'Select reagent'
+        }],
+
         onsubmit(e) {
-          // Insert content when the window form is submitted
-          const kebabbyString = _.kebabCase(e.data.title);
-          editor.insertContent(kebabbyString);
+          let value = e.data.reagent;
+          editor.insertContent(`<span class="widget reagent">${value}</span>&nbsp;`);
+        }
+      });
+
+      let input = $(`#${window._id} input.mce-textbox:first`);
+      $.each(input.parents('.mce-container-body'), (_index, item) => {
+        $(item).css('overflow', 'inherit');
+      });
+
+      input.css('left', 0);
+      input.css('top', 0);
+
+      input.typeahead({
+        minLength: 1
+      }, {
+        display: 'title',
+        templates: {
+          empty: ['<span class="reagent">', 'Unable to find any reagents that match the query', '</span>'].join('\n'),
+          suggestion: Handlebars.compile('<div><span class="reagent">{{title}}</span><span class="in">&nbsp;in</span><span class="inventory">&nbsp;{{inventory}}</span></div>')
+        },
+        source(query, syncCallback, asyncCallback) {
+          let regex = new RegExp(query, 'im');
+          let params = { include: 'inventory', reload: true };
+          editor.settings.store.findAll('reagent', params).then(reagents => {
+            reagents = reagents.filter(reagent => {
+              return regex.test(reagent.get('title')) && !reagent.get('archived');
+            }).map(reagent => {
+              return { id: reagent.get('id'), title: reagent.get('title'), inventory: reagent.get('inventory.title') };
+            });
+            asyncCallback(reagents);
+          });
         }
       });
     }
